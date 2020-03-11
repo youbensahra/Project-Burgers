4e0a98c527195fd9b49d2dad800a93f34574aa63
youbensahra <youssef.bensahra83@gmail.com>
2020-03-11 20:50:29 +0100

    affichage des nutriments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -3,14 +3,18 @@ class Burger < ApplicationRecord
         @elements = []
         $value = []
         @variable = Openfoodfacts::Product.get(code, locale: 'fr').nutriments.to_hash
-        @variable = @variable.to_s
-        @variable.sub!("{", "")
-        @variable.sub!("}", "")
-        @variable.gsub!("\"", " ")
-        @elements = @variable.split(",")
-        for i in @elements
-            $value = $value + "#{i}".split("=>")
-        end
-        return Openfoodfacts::Product.get(code, locale: 'fr').nutriments.to_hash
+        #if @variable.nil?
+        #    error
+         #else 
+            @variable = @variable.to_s
+            @variable.sub!("{", "")
+            @variable.sub!("}", "")
+            @variable.gsub!("\"", " ")
+            @elements = @variable.split(",")
+            for i in @elements
+                $value = $value + "#{i}".split("=>")
+            end
+            return Openfoodfacts::Product.get(code, locale: 'fr').nutriments.to_hash
+        #end
     end
 end
